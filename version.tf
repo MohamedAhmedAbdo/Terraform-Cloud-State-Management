e87a94fef2431ae941f33890d2cad7b09c67383d
mohamed <mohamed.aabdo@fawry.com>
2025-12-10 01:04:27 +0200

    Trying add .terraform.lock.hcl and .terraform/

--- version.tf
+++ version.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.13.3"
+  required_version = ">= 1.13.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,16 +13,16 @@ terraform {
     }
   }
 
-  ## Use for CLI-Driven and local terraform operations
-  # cloud { 
+# Use for CLI-Driven and local terraform operations
+  cloud { 
 
-  #   organization = "amanpathak-devops" 
+    organization = "mohamed-abdo-org" 
 
-  #   workspaces { 
-  #     name = "demo-cli-driven-aws-workspace" 
-  #   } 
-  # } 
+    workspaces { 
+      name = "demo-cli-driven-aws-workspace" 
+    } 
+  } 
 
-  cloud {}
+  # cloud {}
 
 }
